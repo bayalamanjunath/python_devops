bd6a81c14009a06f3452ac5195e51eeb3d8e50f1
iam-manjunath <109402942+bayalamanjunath@users.noreply.github.com>
2024-05-05 20:18:07 +0000

    read

--- practice.py
+++ practice.py
@@ -1,2 +1,16 @@
-#this is branchB 
-readfile 
+# this is shekars branchA 
+# this is gowthams branchB
+# this is chitras branchC
+import os
+import sys
+input_file = sys.argv[1]
+word = sys.argv[2]
+
+def read_file(input_file, word):
+    with open(input_file, 'r') as f:
+        content = (f.read()).split()
+        for line in content:
+            if word in line:
+                print(line)
+
+read_file(input_file, word)
--- readfile.py
+++ readfile.py
@@ -8,5 +8,6 @@ def replace_word(input_file, old_word, new_word):
     with open(input_file, 'w') as f:
         new_content = f.write(replace_content)
         print(new_content)
-replace_word("practice.py", "branchA", "branchB")
+replace_word("practice.py", "branchA", "branchB") 
+
 
