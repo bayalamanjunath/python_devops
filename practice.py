42304199b9d2c04e4144e1ab1e0a0d633f311b60
iam-manjunath <109402942+bayalamanjunath@users.noreply.github.com>
2024-05-07 16:23:27 +0000

    palindrome

--- practice.py
+++ practice.py
@@ -1,16 +1,25 @@
 # this is shekars branchA 
 # this is gowthams branchB
 # this is chitras branchC
-import os
-import sys
-input_file = sys.argv[1]
-word = sys.argv[2]
+# import os
+# import sys
+# input_file = sys.argv[1]
+# word = sys.argv[2]
 
-def read_file(input_file, word):
-    with open(input_file, 'r') as f:
-        content = (f.read()).split()
-        for line in content:
-            if word in line:
-                print(line)
+# def read_file(input_file, word):
+#     with open(input_file, 'r') as f:
+#         content = (f.read()).split()
+#         for line in content:
+#             if word in line:
+#                 print(line)
 
-read_file(input_file, word)
+# read_file(input_file, word)
+#palindrome
+import sys  
+string = sys.argv[1]
+def reverse_string(string):
+
+    r_s = string[::-2] # start : stop : step
+    return r_s
+
+print(reverse_string(string))
