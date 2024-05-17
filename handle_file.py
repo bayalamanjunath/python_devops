e891606fde33ded462a9af98382b3d24039f55ab
iam-manjunath <109402942+bayalamanjunath@users.noreply.github.com>
2024-05-17 19:35:16 +0000

    fike

--- handle_file.py
+++ handle_file.py
@@ -3,6 +3,11 @@ import os
 import shutil
 # file_name = sys.argv[1]
 # file = open(file_name,'r')
+# file = open(file_name,'w')
+# file = open(file_name,'a')
+# file.wirte('overwrite the content in the file')
+# file.wirte('\noverwrite the content in the file\n')
+# file.append('only adds the contents')
 # with open(file_name, 'r') as file:
 # content = file.read()
 # content = file.readline()
