fe48f41d493872a8c231b2b9800e9a905bd3cd0f
iam-manjunath <109402942+bayalamanjunath@users.noreply.github.com>
2024-05-17 19:14:32 +0000

    file

---
+++ handle_file.py
@@ -0,0 +1,21 @@
+import sys
+import os
+import shutil
+# file_name = sys.argv[1]
+# file = open(file_name,'r')
+# with open(file_name, 'r') as file:
+# content = file.read()
+# content = file.readline()
+# content = file.readlines()
+# print(content)
+# file.close()
+# os.mkdir('folder1')
+# a = os.listdir('.')
+# print(a)
+# x = os.path.exists('kubernetes')
+# print(x)
+# a = os.removedirs('folder1')
+# print(a)
+# a = shutil.ReadError('test.py')
+# shutil.rmtree
+# print(a)
